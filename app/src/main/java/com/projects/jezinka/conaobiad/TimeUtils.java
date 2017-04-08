d4a7bc17c22921b10df61e5b9e0cf4c5efe7d3a8
jezinka <jezina1989@gmail.com>
2017-04-08 11:33:42 +0200

    add test to test clearing time

--- app/src/main/java/com/projects/jezinka/conaobiad/TimeUtils.java
+++ app/src/main/java/com/projects/jezinka/conaobiad/TimeUtils.java
@@ -20,17 +20,21 @@ public class TimeUtils {
         Calendar calendarInstance = Calendar.getInstance();
         calendarInstance.setTime(date);
 
-        calendarInstance.set(Calendar.HOUR_OF_DAY, 0);
-        calendarInstance.set(Calendar.MINUTE, 0);
-        calendarInstance.set(Calendar.SECOND, 0);
-        calendarInstance.set(Calendar.MILLISECOND, 0);
+        clearTime(calendarInstance);
 
         int dayOfWeek = calendarInstance.get(Calendar.DAY_OF_WEEK);
         if (dayOfWeek == Calendar.SATURDAY) {
-            return date;
+            return calendarInstance.getTime();
         }
 
         calendarInstance.add(Calendar.DAY_OF_WEEK, -dayOfWeek);
         return calendarInstance.getTime();
+    }
+
+    private static void clearTime(Calendar calendarInstance) {
+        calendarInstance.set(Calendar.HOUR_OF_DAY, 0);
+        calendarInstance.set(Calendar.MINUTE, 0);
+        calendarInstance.set(Calendar.SECOND, 0);
+        calendarInstance.set(Calendar.MILLISECOND, 0);
     }
 }
--- app/src/test/groovy/com/projects/jezinka/conaobiad/UnitTests.groovy
+++ app/src/test/groovy/com/projects/jezinka/conaobiad/UnitTests.groovy
@@ -15,18 +15,20 @@ class UnitTests extends Specification {
         resultDate == expectedDate
 
         where:
-        date                 | expectedDate
-        new Date(117, 2, 4)  | new Date(117, 2, 4)
-        new Date(117, 2, 13) | new Date(117, 2, 11)
-        new Date(117, 2, 11) | new Date(117, 2, 11)
-        new Date(117, 2, 12) | new Date(117, 2, 11)
-        new Date(117, 2, 14) | new Date(117, 2, 11)
-        new Date(117, 2, 15) | new Date(117, 2, 11)
-        new Date(117, 2, 16) | new Date(117, 2, 11)
-        new Date(117, 2, 17) | new Date(117, 2, 11)
+        date                            | expectedDate
+        new Date(117, 2, 4)             | new Date(117, 2, 4)
+        new Date(117, 2, 13)            | new Date(117, 2, 11)
+        new Date(117, 2, 11)            | new Date(117, 2, 11)
+        new Date(117, 2, 12)            | new Date(117, 2, 11)
+        new Date(117, 2, 14)            | new Date(117, 2, 11)
+        new Date(117, 2, 15)            | new Date(117, 2, 11)
+        new Date(117, 2, 16)            | new Date(117, 2, 11)
+        new Date(117, 2, 17)            | new Date(117, 2, 11)
 
-        new Date(117, 2, 3)  | new Date(117, 1, 25)
-        new Date(117, 0, 1)  | new Date(116, 11, 31)
+        new Date(117, 2, 3)             | new Date(117, 1, 25)
+        new Date(117, 0, 1)             | new Date(116, 11, 31)
+
+        new Date(117, 3, 8, 11, 12, 32) | new Date(117, 3, 8, 0, 0, 0)
     }
 
     def "GetSaturdayDate no exception thrown for null parameter"() {
@@ -36,5 +38,18 @@ class UnitTests extends Specification {
 
         then:
         noExceptionThrown()
+    }
+
+    def "GetSaturdayDate time always cleared"() {
+        when:
+        def result = TimeUtils.getSaturdayDate()
+        Calendar calendarInstance = Calendar.getInstance()
+        calendarInstance.setTime(result)
+
+        then:
+        noExceptionThrown()
+        assert calendarInstance.get(Calendar.HOUR) == 0
+        assert calendarInstance.get(Calendar.MINUTE) == 0
+        assert calendarInstance.get(Calendar.SECOND) == 0
     }
 }
