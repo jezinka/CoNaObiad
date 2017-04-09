c13cec15d9f03a350ddb86078d49eb3c950a1068
jezinka <jezina1989@gmail.com>
2017-04-09 14:26:21 +0200

    change button title regarding of add/edit action

--- app/src/main/java/com/projects/jezinka/conaobiad/MealListActivity.java
+++ app/src/main/java/com/projects/jezinka/conaobiad/MealListActivity.java
@@ -106,7 +106,7 @@ public class MealListActivity extends AppCompatActivity {
         }
         builder.setView(input);
 
-        builder.setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(meal == null ? R.string.add : R.string.edit, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 mealName = input.getText().toString();
