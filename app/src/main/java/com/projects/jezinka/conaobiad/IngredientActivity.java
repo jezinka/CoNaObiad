8a7deb109bddfd3e65783860a4201a6ad25f18d8
jezinka <jezina1989@gmail.com>
2017-04-21 19:27:07 +0200

    introduction of ingredient

---
+++ app/src/main/java/com/projects/jezinka/conaobiad/IngredientActivity.java
@@ -0,0 +1,13 @@
+package com.projects.jezinka.conaobiad;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class IngredientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient);
+    }
+}
---
+++ app/src/main/java/com/projects/jezinka/conaobiad/model/IngredientContract.java
@@ -0,0 +1,78 @@
+package com.projects.jezinka.conaobiad.model;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+import com.projects.jezinka.conaobiad.CoNaObiadDbHelper;
+
+import java.util.ArrayList;
+
+public class IngredientContract extends BaseTable implements BaseColumns {
+
+    String COLUMN_NAME_NAME;
+    private String SQL_GET_ALL_RECORD;
+
+    public IngredientContract() {
+        this.TABLE_NAME = "ingredient";
+        this.COLUMN_NAME_NAME = "name";
+
+        this.SQL_CREATE_ENTRIES = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY," +
+                COLUMN_NAME_NAME + " TEXT)";
+
+        this.SQL_GET_ALL_RECORD = "select " + this._ID + ", " + this.COLUMN_NAME_NAME +
+                " from " + this.TABLE_NAME +
+                " order by " + this.COLUMN_NAME_NAME + " COLLATE NOCASE";
+    }
+
+    public boolean insert(Context context, String name) {
+        String tableName = this.getTableName();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+
+        CoNaObiadDbHelper helper = new CoNaObiadDbHelper(context);
+        helper.insert(tableName, contentValues);
+        return true;
+    }
+
+    public boolean update(Context context, String name, Ingredient ingredient) {
+        String tableName = this.getTableName();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+
+        CoNaObiadDbHelper helper = new CoNaObiadDbHelper(context);
+        helper.update(tableName, contentValues, ingredient.getId());
+        return true;
+    }
+
+    public ArrayList<Ingredient> getAllIngredients(SQLiteOpenHelper helper) {
+        ArrayList<Ingredient> array_list = new ArrayList<>();
+
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor res = db.rawQuery(this.SQL_GET_ALL_RECORD, null);
+        if (res != null && res.getCount() > 0) {
+            res.moveToFirst();
+
+            do {
+                String name = res.getString(res.getColumnIndex(this.COLUMN_NAME_NAME));
+                int id = res.getInt(res.getColumnIndex(this._ID));
+                array_list.add(new Ingredient(id, name));
+            } while (res.moveToNext());
+        }
+
+        db.close();
+
+        return array_list;
+    }
+
+    public Ingredient[] getAllIngredientsArray(SQLiteOpenHelper helper) {
+        ArrayList<Ingredient> result = getAllIngredients(helper);
+        return result.toArray(new Ingredient[result.size()]);
+    }
+}
