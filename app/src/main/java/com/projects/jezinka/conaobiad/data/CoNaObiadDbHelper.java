b653908f8b71d6b218cbe05d5156825088170798
jezinka <jezina1989@gmail.com>
2017-07-01 10:16:27 +0200

    fix on _ cascade

--- app/src/main/java/com/projects/jezinka/conaobiad/data/CoNaObiadDbHelper.java
+++ app/src/main/java/com/projects/jezinka/conaobiad/data/CoNaObiadDbHelper.java
@@ -29,6 +29,12 @@ public class CoNaObiadDbHelper extends SQLiteOpenHelper {
     }
 
     @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        db.execSQL("PRAGMA foreign_keys=ON");
+    }
+
+    @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(MealContract.getCreateEntriesQuery());
         db.execSQL(DinnerContract.getCreateEntriesQuery());
