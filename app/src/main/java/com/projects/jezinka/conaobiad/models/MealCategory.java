c0eb6f0abb850ce9ffe9e89ae95a1137725528f8
jezinka <jezina1989@gmail.com>
2017-09-14 09:23:57 +0200

    remove duplicate id

---
+++ app/src/main/java/com/projects/jezinka/conaobiad/models/MealCategory.java
@@ -0,0 +1,15 @@
+package com.projects.jezinka.conaobiad.models;
+
+public class MealCategory {
+    private long id;
+    private String name;
+
+    public MealCategory() {
+        super();
+    }
+
+    public MealCategory(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
